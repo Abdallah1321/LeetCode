64d02ce7dcaa1ad2159aa08df1fb0c1a3fac6f3b
Abdallah Ibrahim <78796081+Abdallah1321@users.noreply.github.com>
2024-04-19 15:06:54 +0200

    Time: 219 ms (64.46%), Space: 27.2 MB (54.18%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+            grid[i][j] = '0'  # mark as visited
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        
+        num_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    num_islands += 1
+                    dfs(i, j)
+        
+        return num_islands
